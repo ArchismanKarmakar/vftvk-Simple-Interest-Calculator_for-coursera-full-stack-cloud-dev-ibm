413aced7bd02948843b42d4b25d7739e95b50e89
Archisman Karmakar <92569441+ArchismanKarmakar@users.noreply.github.com>
2021-12-20 02:04:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,17 @@ function compute()
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
+
+    if ((principal<=0)||(rate<=0)||(years<0)){
+        alert("Please enter a positive value");
+        
+    }
+    else{
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
     // var p = document.getElementById("output").innerHTML;
     document.getElementById("result").innerHTML="If you deposit " + principal +"\<br\>"+"at an interest rate of "+rate+"%\<br\>"+"You will receive an amount of "+interest+"\<br\>"+"in the year "+year;
+    }
 }
         
 function updateRate() 
