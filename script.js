28efb0c57b401605bdab9628e90fc6fd3380aa8b
Archisman Karmakar <92569441+ArchismanKarmakar@users.noreply.github.com>
2021-12-20 02:06:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function compute()
+{
+    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    if ((principal<=0)||(rate<=0)||(years<0)){
+        alert("Please enter a positive value");
+        
+    }
+    else{
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    // var p = document.getElementById("output").innerHTML;
+    document.getElementById("result").innerHTML="If you deposit " + principal +"\<br\>"+"at an interest rate of "+rate+"%\<br\>"+"You will receive an amount of "+interest+"\<br\>"+"in the year "+year;
+    }
+}
+        
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
+}
